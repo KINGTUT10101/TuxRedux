ed37ab0fcbd91cf9038a3394dc9142f0fc050f41
KINGTUT10101 <kingtut10101@gmail.com>
2024-06-27 23:25:30 -0500

    Fix label debug boundary color

--- tux/components/label.lua
+++ tux/components/label.lua
@@ -12,6 +12,8 @@ function component.draw (tux, opt)
     tux.core.rect (opt.slices, opt.colors, "normal", tux.core.unpackCoords (opt))
     tux.core.drawImage (opt.image, opt.scale, opt.align, opt.valign, opt.padding, tux.core.unpackCoords (opt))
     tux.core.print (opt.text, opt.align, opt.valign, opt.padding, opt.font, opt.colors, "normal", tux.core.unpackCoords (opt))
+
+    tux.core.debugBoundary (opt.state, tux.core.unpackCoords (opt))
 end
 
 return component
