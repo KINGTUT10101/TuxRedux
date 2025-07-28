c58283ff6f2a6da9d17d1930f6c0099c7711d185
KINGTUT10101 <kingtut10101@gmail.com>
2025-07-28 13:54:44 -0500

    Add expandColor method

--- tux/style.lua
+++ tux/style.lua
@@ -2,3 +2,19 @@ local libPath = (...):match("(.+)%.[^%.]+$") .. "."
 local copyTable = require(libPath .. "helpers.copyTable")
 
 local tux = require(libPath .. "tux")
+
+--- Expands a two-color tuple into a full color table.
+--- @param fg table<number> The foreground color (table or tuple)
+--- @param bg table<number> The background color (table or tuple)
+--- @return table<string, table<number>> colors A table containing the expanded color values
+function tux.style.expandColor (fg, bg)
+    local state = {
+        fg = copyTable (fg),
+        bg = copyTable (bg),
+    }
+    return {
+        normal  = state,
+        hover = state,
+        held  = state,
+    }
+end
